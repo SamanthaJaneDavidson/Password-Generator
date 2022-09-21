602723993832e708b3f075b375532dcd8d031bbb
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-09-21 12:10:27 -0400

    Updated variables

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //var pwLength = range(8, 128);
-var pwLowercase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-var pwUppercase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-//var pwNumeric = range(0, 9);
-var pwSpecial = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var pwLowercase = "abcdefghijklmnopqrstuvwxyz"
+var pwUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var pwNumeric = "123456789"
+var pwSpecial = "!@#$%^&*"
 
 var pwLength = prompt("How many characters would you like in your password? Please choose between 8 and 128).")
-var pwLowercase = confirm("Would you like lower case characters in your password?")
-var pwUppercase = confirm("Would you like upper case characters in your password?")
-var pwNumeric = confirm("Would you like numeric characters in your password?")
-var pwSpecial = confirm("Would you like special characters in your pasword?")
+var pwLowercase = confirm("Click ok if you would like lower case characters in your password?")
+var pwUppercase = confirm("Click ok if you would like upper case characters in your password?")
+var pwNumeric = confirm("Click ok if you would like numeric characters in your password?")
+var pwSpecial = confirm("Click ok if you would like special characters in your pasword?")
 
 
+
+//pwLowercase[Math.floor(Math.random() * (pwLowercase.length - 0) + 0)]
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+	var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
