c0af520487d305baf203dd972d953884d734507b
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-09-22 00:09:06 -0400

    Added notes to myself for later

--- script.js
+++ script.js
@@ -21,16 +21,29 @@ function writePassword() {
 
 function generatePassword() {
   var output = "";
-  pwPool = "abc";
+  pwPool = "";
   var pwLength = parseInt(prompt("How many characters would you like in your password? Please choose between 8 and 128)."))
   var pwLowercase = confirm("Click ok if you would like lower case characters in your password?")
   var pwUppercase = confirm("Click ok if you would like upper case characters in your password?")
   var pwNumeric = confirm("Click ok if you would like numeric characters in your password?")
   var pwSpecial = confirm("Click ok if you would like special characters in your pasword?")
-for(var i=0; i<pwLength; i++) {
+
+
+  if (pwLowercase) {
+    console.log //how do I tell pwPool to include lowercase? 
+  } if (pwUppercase) {
+    console.log
+  } if (pwNumeric) {
+    console.log
+  } if (pwSpecial) {
+    console.log 
+  } else //error must choose one? 
+
+  for (var i = 0; i < pwLength; i++) {
   console.log(pwPool[Math.floor(Math.random() * (pwPool.length - 0) + 0)]); 
 
 }
+
   return output;
 }
 
