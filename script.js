3bee5ff7fa775061f6bcdc9932dc17047b26a5d9
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-09-23 17:16:57 -0400

    Updated js file names

--- script.js
+++ script.js
@@ -21,45 +21,41 @@ function writePassword() {
 
 function generatePassword() {
   pwPool = "";
+    //How can I use the variable names here instead of manaully adding the values? 
   var pwLength = parseInt(prompt("How many characters would you like in your password? Please choose between 8 and 128)."))
+    if (pwLength < 8 || pwLength > 128){
+        alert("You must choose a number between 8 and 128");
+        var pwLength = parseInt(prompt("How many characters would you like in your password? Please choose between 8 and 128)."))} 
+
   var pwLowercase = confirm("Click ok if you would like lower case characters in your password?")
+    if (pwLowercase) {
+        var pwPool = "abcdefghijklmnopqrstuvwxyz" + pwPool;}
+
   var pwUppercase = confirm("Click ok if you would like upper case characters in your password?")
+    if (pwUppercase) {
+        var pwPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + pwPool;}
+
   var pwNumeric = confirm("Click ok if you would like numeric characters in your password?")
+    if (pwNumeric) {
+        var pwPool = "123456789" + pwPool;}
+  
   var pwSpecial = confirm("Click ok if you would like special characters in your pasword?")
+    if (pwSpecial) {
+        var pwPool = "!@#$%^&*" + pwPool;}
 
-
-  //How can I use the variable names here instead of manaully adding the values? 
-  
-  // if (pwLength < 8 || pwLength > 128); {
-  //   alert("Please choose a number between 8 and 128.");
-  //     generatePassword()
-  //   }
-  
-  if (pwLowercase) {
-     var pwPool = "abcdefghijklmnopqrstuvwxyz" + pwPool;
-
-  } if (pwUppercase) {
-     var pwPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + pwPool;
-   
-  } if (pwNumeric) {
-     var pwPool = "123456789" + pwPool;
-
-  } if (pwSpecial) {
-     var pwPool = "!@#$%^&*" + pwPool;
-  
-  } while (!pwLowercase && !pwUppercase && !pwNumeric && !pwSpecial) {
-      invalidResp = alert("You must choose at least one character type.");
-      generatePassword() //never generates password just keeps asking the questions
-    }
-  
-  var output = ""; //I used to have this at the top of the function but it didn't work there. Why?
-    for (var i = 0; i < pwLength; i++) { 
-      output += (pwPool[Math.floor(Math.random() * (pwPool.length - 0) + 0)]);  
-    } 
-  
-  return output; 
-
+    else if (!pwLowercase && !pwUppercase && !pwNumeric && !pwSpecial) {
+    alert("You must choose at least one character type.");
+    return generatePassword()
   }
+
+    var output = ""; 
+        for (var i = 0; i < pwLength; i++) { 
+        output += (pwPool[Math.floor(Math.random() * (pwPool.length - 0) + 0)]);  
+        }
+         
+    return output;
+    
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
